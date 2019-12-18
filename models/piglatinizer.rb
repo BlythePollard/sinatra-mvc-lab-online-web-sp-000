a9b381d1a10b74b1b971be1260b2f91f29cc686d
BlythePollard <github email address>
2019-12-17 20:28:22 -0700

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,40 @@
+class PigLatinizer
+
+    #do I need attr_reader?
+
+    def text_define(text)
+        if text.split(/\s/).length == 1
+            pig_latinize_word(text)
+        elsif text.split(/\s/).length > 1  
+            pig_latinize_string(text)
+        end
+    end
+
+    def pig_latinize_word(text)
+        #first_letter = text.split('')[0]
+        if ["a", "e", "i", "o", "u", "h"].include?(text.split('')[0])
+            "#{text}way"
+        else 
+            #second_letter = text.split('')[1]
+            #third_letter = text.split('')[2]
+            #fourth_letter = text.split('')[3]
+            if ["a", "e", "i", "o", "u", "h", "y"].include?(text.split('')[1])
+                "#{text[1..-1]}#{text.split('')[0]}ay"
+            elsif ["a", "e", "i", "o", "u", "h", "y"].include?(text.split('')[2])
+                "#{text[2..-1]}#{text.split('')[0]}#{text.split('')[1]}ay" 
+            elsif ["a", "e", "i", "o", "u", "h", "y"].include?(text.split('')[3])
+                "#{text[3..-1]}#{text.split('')[0]}#{text.split('')[1]}#{text.split('')[2]}ay"
+            end
+        end
+    end
+    #th, ch, sh combos have to stay together
+
+    def pig_latinize_string(text)
+        a = text.split(' ')
+        b= a.map {|word| pig_latinize_word(word)}
+        b.join(' ')
+    end
+
+end
+
+
